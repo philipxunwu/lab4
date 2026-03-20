6ef6a39db2b876b531a0fe5332de62fac17ff461
philipxunwu <pwuxun@gmail.com>
2026-03-20 19:26:10 -0400

    apsdaofi

--- lab4/axis_data_fifo.v
+++ lab4/axis_data_fifo.v
@@ -54,9 +54,9 @@ reg [AXIS_DATA_WIDTH+1-1:0] rd_data_reg = {AXIS_DATA_WIDTH+1{1'b0}};
 reg rd_axis_vld_reg = 1'b0, rd_axis_vld_next;
 
 // TODO: check for full status based on comparison between wr_ptr_gray_reg and rd_ptr_gray_sync2_reg
-wire full = (wr_ptr_gray_reg[ADDR_WIDTH] != rd_ptr_gray_sync2_reg[ADDR_WIDTH]) &&
-             (wr_ptr_gray_reg[ADDR_WIDTH-1] != rd_ptr_gray_sync2_reg[ADDR_WIDTH-1]) &&
-             (wr_ptr_gray_reg[ADDR_WIDTH-2:0] == rd_ptr_gray_sync2_reg[ADDR_WIDTH-2:0]);
+wire full = (wr_ptr_gray_reg[ADDR_WIDTH]   != rd_ptr_gray_sync2_reg[ADDR_WIDTH]) &&
+              (wr_ptr_gray_reg[ADDR_WIDTH-1] != rd_ptr_gray_sync2_reg[ADDR_WIDTH-1]) &&
+              (wr_ptr_gray_reg[ADDR_WIDTH-2:0] == rd_ptr_gray_sync2_reg[ADDR_WIDTH-2:0]);
 
 // TODO: check for empty status based on comparison between wr_ptr_gray_reg and rd_ptr_gray_sync2_reg
 wire empty = (wr_ptr_gray_reg == rd_ptr_gray_sync2_reg);
@@ -67,7 +67,7 @@ reg read;
 reg store_output;
 
 // TODO: complete the logic for wr_axis_rdy
-assign wr_axis_rdy = ~full; 
+assign wr_axis_rdy = ~full;
 
 assign rd_axis_vld = rd_axis_vld_reg;
 
@@ -109,7 +109,7 @@ always @* begin
     //TODO: complete the write logic; hint should we always execute the the following signal assignments? or is there a condition that we should check?
     
     // input data valid
-    if (wr_axis_vld && wr_axis_rdy) begin
+    if (wr_axis_vld && ~full) begin
         // not full, perform write
         write = 1'b1;
         wr_ptr_next = wr_ptr_reg + 1;
@@ -166,13 +166,13 @@ always @* begin
     //TODO: complete the output read logic;
 
     // output data not valid OR currently being transferred
-    if (~empty && (~mem_read_data_valid_reg || rd_axis_rdy)) begin
+    if (~empty && rd_axis_rdy) begin
         // not empty, perform read
         read = 1'b1;
         mem_read_data_valid_next = 1'b1;
         rd_ptr_next = rd_ptr_reg + 1;
         rd_ptr_gray_next = rd_ptr_next ^ (rd_ptr_next >> 1);
-    end else begin
+    end else if (rd_axis_rdy) begin
         // empty, invalidate
         mem_read_data_valid_next = 1'b0;
     end
@@ -198,15 +198,16 @@ end
 
 // Output register
 always @* begin
-    store_output = ~rd_axis_vld_reg || rd_axis_rdy;
+    store_output = 1'b0;
 
     rd_axis_vld_next = rd_axis_vld_reg;
 
     //TODO: complete the output register logic;
     
-    if (store_output) begin
+    if (rd_axis_rdy) begin 
+        store_output = 1'b1;
         rd_axis_vld_next = mem_read_data_valid_reg;
-    end
+    end 
 end
 
 always @(posedge rd_aclk) begin
